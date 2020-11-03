5362a3faced45effdd726716746b1cd579f55587
josedev-union <josebarato321@gmail.com>
2020-11-03 23:45:17 +0200

    First commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+
+IMAGE_NAME=stakater/nexus3
+IMAGE_VERSION=3.28.1-keycloak_plugin0.4.0
+
+docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} \
+             -f "${DIR}/Dockerfile" "${DIR}"
