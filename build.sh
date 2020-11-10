d70dd806074a20a13fe866739a57bb9d7e556395
josedev-union <70741025+josedev-union@users.noreply.github.com>
2020-11-10 16:56:39 +0100

    bump to 0.4.1

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
 IMAGE_NAME=stakater/nexus3
-IMAGE_VERSION=3.28.1-keycloak_plugin0.4.0
+IMAGE_VERSION=3.28.1-keycloak_plugin0.4.1
 
 docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} \
              -f "${DIR}/Dockerfile" "${DIR}"
